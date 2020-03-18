1c18e673ccf0b457cd042f976196150d3af7b810
Viaje9 <h99y5246cv@gmail.com>
2020-03-18 23:56:52 +0800

    Revert "2020/03/18"
    This reverts commit 90d110d253f7b12549df928e2ba7af92f102200b.

---
+++ stockbackend/storage/framework/views/12eb8e8518680970a436c736b87358652c05f504.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html  >
+<head>
+    <!-- Site made with Mobirise Website Builder v4.12.1, https://mobirise.com -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Mobirise v4.12.1, mobirise.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <link rel="shortcut icon" href="<?php echo e(asset('assets/images/logo2.png')); ?>" type="image/x-icon">
+    <meta name="description" content="">
+
+    <title>Home</title>
+    <link rel="stylesheet" href="<?php echo e(asset('assets/web/assets/mobirise-icons/mobirise-icons.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/bootstrap/css/bootstrap.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/bootstrap/css/bootstrap-grid.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/bootstrap/css/bootstrap-reboot.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/facebook-plugin/style.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/tether/tether.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/dropdown/css/style.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/theme/css/style.css')); ?>">
+    <link rel="preload" as="style" href="<?php echo e(asset('assets/mobirise/css/mbr-additional.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/mobirise/css/mbr-additional.css')); ?>" type="text/css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <?php echo $__env->yieldContent('css'); ?>
+</head>
+
+<body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            基本資料
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t05st03">公司基本資料</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t79sb02">重要子公司基本資料</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t16sn02">股權分散表</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/stapap1">董監事持股餘額明細資料</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t93sb06_1">持股10%以上大股東最近異動情形</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            財務報表(採IFRS後)
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t57sb01_q1">財務報告書</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t163sb01">財務報告公告</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t163sb15">簡明綜合損益表(四季)</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t163sb16">簡明資產負債表(四季)</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t163sb17">簡明綜合損益表(三年)</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t163sb18">簡明資產負債表(四季)</a>
+                            <a class="dropdown-item" href="#">採用IFRSs後之月營業收入資訊</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            重大訊息
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t05st01">歷史重大訊息</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t05st02">當日重大訊息</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t100sb07_1">法說會</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            信用評等
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t191sb01">上市公司信用評等</a>
+                            <a class="dropdown-item" href="https://mops.twse.com.tw/mops/web/t109sb02_w1">證券商信用評等</a>
+                        </div>
+                    </li>
+                </ul>
+                <!-- <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form> -->
+            </div>
+        </nav>
+
+        <?php echo $__env->yieldContent('content'); ?>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+
+
+    <script src="<?php echo e(asset('assets/popper/popper.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/bootstrap/js/bootstrap.min.js')); ?>"></script>
+    <script src="https://connect.facebook.net/en_US/sdk.js') }}#xfbml=1&version=v2.5"></script>
+    <script src="https://apis.google.com/js/plusone.js') }}"></script>
+    <script src="<?php echo e(asset('assets/facebook-plugin/facebook-script.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/tether/tether.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/smoothscroll/smooth-scroll.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/dropdown/js/nav-dropdown.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/dropdown/js/navbar-dropdown.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/touchswipe/jquery.touch-swipe.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/theme/js/script.js')); ?>"></script>
+
+    <?php echo $__env->yieldContent('js'); ?>
+
+</body>
+</html>
+<?php /**PATH C:\Users\lulu\Documents\GitHub\StockIntegrationPlatform\stockbackend\resources\views//layouts/nav.blade.php ENDPATH**/ ?>
